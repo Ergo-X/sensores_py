137cc7d5b149321ee4f2a4efeb6d3e8362c82a65
Paulo Brandão <paulobrandaoofficial@gmail.com>
2020-09-29 16:25:30 -0300

    Sensores funcionando

---
+++ Scripts/Decibel.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as gpio
+import time
+import wiringpi
+
+from datetime import datetime, timedelta
+from sys import stdout
+from time import sleep
+
+class Decibel:
+    def medir_decibeis(self):
+        gpio.setmode(gpio.BCM) 
+        gpio.setup(17, gpio.IN, pull_up_down = gpio.PUD_DOWN)
+        pino_sensor = 17
+        amostraTempo = timedelta(milliseconds=10)
+        ultimoTempo = timedelta(milliseconds=0)
+        tempoDecorrido = 0
+        valorExibido = 0
+        tempoAtual = timedelta(milliseconds=0)
+
+        def millis(tempoAtual):
+            tempoAtual = tempoAtual + timedelta(milliseconds=0.1)
+            sleep(0.001)
+            return tempoAtual
+
+        while True:    
+          tempoAtual = millis(tempoAtual);
+          tempoDecorrido = tempoAtual - ultimoTempo  
+          
+          if (gpio.input(pino_sensor) == 0):
+            valorExibido = valorExibido + 1
+
+          if (tempoDecorrido > amostraTempo):
+            #print(valorExibido+30)
+            return (valorExibido+30)
+            valorExibido = 0
+            ultimoTempo = tempoAtual 
+
+
+
+
+
---
+++ Scripts/Program.py
@@ -0,0 +1,63 @@
+import os
+import asyncio
+import busio
+import json
+import time
+import board
+import adafruit_ahtx0
+import adafruit_tsl2561
+from Decibel import Decibel
+from azure.iot.device.aio import IoTHubDeviceClient
+
+# Declara a String de conexão do Device
+connection_string = "HostName=Senai132IotRaspberry.azure-devices.net;DeviceId=rasp1;SharedAccessKey=+eFtg2cGnvbD5xM6HI7BKIoSBuZCJsrVBMiS4tP1LI0="
+# Declara quais são as portas para os sensores i2c
+i2c = busio.I2C(board.SCL, board.SDA)
+# Declara o driver do sensor de temperatura e umidadade AHT10 como aht
+aht = adafruit_ahtx0.AHTx0(i2c)
+# Declara o driver do sensor de luminosidade TSL2561 como tsl
+tsl = adafruit_tsl2561.TSL2561(i2c)
+# Declara a variável lux como 0
+lux = 0
+
+async def main():
+    # Pega a String de Conexão das variáveis de ambiente
+    conn_str = os.getenv(str(connection_string))
+    # Cria uma instância do Device com a String de Conexão
+    device_client = IoTHubDeviceClient.create_from_connection_string(connection_string)
+    # Conecta ao Device
+    await device_client.connect()
+    # Enquanto for True ele vai mandar a mensagem para o Device no IotHub
+    while True:
+        # Confere se o valor do sensor de luminosidade não é None
+        # Se for None ele declara o valor de lux como 0
+        if tsl.lux == None:
+            lux = 0
+            print(lux)
+        # Se não for None ele pega o valor de leitura do sensor e declara dentro da variável lux
+        else:
+            lux = tsl.lux
+        
+        # Instancia a classe de Decibéis
+        db = Decibel()    
+            
+        # Monta em json a mensagem que vai ser enviada ao IotHub com as leituras dos sensores
+        message = { "temperatura": str(round(float(aht.temperature),2)),
+                    "umidade": str(round(float(aht.relative_humidity),2)),
+                    "luminosidade": str(round(float(lux),2)),
+                    "decibeis": str(round(float(db.medir_decibeis()),2))}
+        
+        # Envia para o Device a mensagem que foi montada
+        await device_client.send_message(str(message))
+        # Mostra a mensagem
+        print(message)
+        # Espera dois segundos
+        time.sleep(2)
+
+    # Desconecta do Device
+    await device_client.disconnect()
+
+# Diz para o código para rodar a função main() assim desde que roda a primeira vez
+if __name__ == "__main__":
+    asyncio.run(main())
+
